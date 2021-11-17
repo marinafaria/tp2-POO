fbae7ddb64b391770146e48985bef0d537c428b7
Guilherme Santos <51834354+guilhas-cpu@users.noreply.github.com>
2021-11-17 19:24:00 -0300

    correção do matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,29 +1,84 @@
-// main.cpp
+// matrix.cpp
 #include "matrix.h"
 using namespace std;
 
-int main()
-{
-    Matrix Y(3, 2);
-    Matrix X(4, 1);
+// contrutor default - cria uma matriz vazia com nRows = nCols = 0  
+Matrix::Matrix() {
+    nCols = 0;
+    nRows = 0;
+    m = new double* [nRows];
+}
 
-    std::cout << "Y:: " << std::endl;
-    Y.print();
-    std::cout << "Numero de linhas de Y:: " << Y.getRows() << endl;
-    std::cout << "Numero de colunas de Y:: " << Y.getCols() << endl;
 
-    std::cout << "Z é transposta de Y:: " << std::endl;
-    Matrix Z = Y.transpose();
-    cout << "cheguei aqui" << endl;
-    Z.print();
-    std::cout << "Numero de linhas de Z:: " << Z.getRows() << endl;
-    std::cout << "Numero de colunas de Z:: " << Z.getCols() << endl;
+// contrutor parametrico 1 - cria uma matriz com nRows  = rows, nCols = cols e 
+// com todos os elementos iguais a elem (double)
+Matrix::Matrix(int rows, int cols, double elem) {
+    nRows = rows;
+    nCols = cols;
+    aloca_matriz();
+    for (int i = 0; i < nRows; i++) {
+        for (int j = 0; j < nCols; j++) {
+            m[i][j] = elem;
+        }
+    }
+}
 
-    std::cout << std::endl << "X:: " << std::endl;
-    X.print();
 
-    std::cout << "Numero de linhas de X:: " << X.getRows() << endl;
-    std::cout << "Numero de colunas de X:: " << X.getCols() << endl;
+// destrutor
+//Matrix::~Matrix() {
+//    for (int i = 0; i < this->nRows; i++)
+//        delete[] this->m[i];
+//    delete[] this->m;
+//    this->m = NULL; 
+//    this->nCols = 0;
+//    this->nRows = 0;
+//}
 
-    return 0;
+void Matrix::aloca_matriz() {
+    m = new double* [nRows];
+    for (int i = 0; i < nRows; i++)
+        m[i] = new double[nCols];
+}
+
+
+// obtem o numero de linhas
+int Matrix::getRows() const {
+    return nRows;
+}
+
+// obtem o numero de colunas
+int Matrix::getCols() const {
+    return nCols;
+}
+
+// retorna uma matriz transposta
+Matrix Matrix::transpose() {
+    Matrix aux(nCols, nRows, 0);
+    for (int i = 0; i < aux.nRows; i++) {
+        for (int j = 0; j < aux.nCols; j++) {
+            aux.m[i][j] = this->m[j][i];
+        }
+    }
+    this->nCols = aux.nCols;
+    this->nRows = aux.nRows;
+    this->aloca_matriz();
+    for (int i = 0; i < aux.nRows; i++) {
+        for (int j = 0; j < aux.nCols; j++) {
+            this->m[i][j] = aux.m[i][j];
+        }
+    }
+
+
+    return *this;
+}
+
+// imprime o conteudo da matriz
+void Matrix::print() const {
+    cout << m[0][0] << endl;
+    for (int i = 0; i < nRows; i++) {
+        for (int j = 0; j < nCols; j++) {
+            cout << this->m[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
